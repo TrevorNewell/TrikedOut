d91f8a970a08fc0540a932217a4f66bf43807a5e
TrevorNewell <trevor.newell@utah.edu>
2016-10-29 14:50:06 -0600

    Added some small changes to character selection screen.
    I added spot lights to the character selection screen.  The image on the
    tracks appears dark for some reason despite being on a UI object.  I put
    a point light on them for now as a temporary fix.  I can investigate
    later.

---
+++ Assets/Scripts/Screen/EnableDisableOnHighlight.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+// Attach to a button, and set the parameter to enable when highlighted and disable when not highlighted
+public class EnableDisableOnHighlight : MonoBehaviour, ISelectHandler, IDeselectHandler
+{
+    public GameObject objectToTurnOnOff;
+    public Button theButton;
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        Debug.Log("This bitch " + this.gameObject.name + " was selected.");
+        objectToTurnOnOff.SetActive(true);
+    }
+
+    public void OnDeselect(BaseEventData eventData)
+    {
+        Debug.Log("Deselected this " + this.gameObject.name + " bitch.");
+
+        objectToTurnOnOff.SetActive(false);
+    }
+
+	// Use this for initialization
+	void Start ()
+    {
+        theButton = gameObject.GetComponent<Button>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
