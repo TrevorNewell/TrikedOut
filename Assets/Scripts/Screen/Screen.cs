fd5f81ccce1b59a13bbf7b31c0dd13b1f0d1b454
TrevorNewell <trevor.newell@utah.edu>
2016-10-28 18:57:24 -0600

    Menus are MOSTLY done.  I have a little more work to do..
    Menus are mostly done, I have a little more work to do, and I'll
    probably do that tomorrow or Sunday.  :)

---
+++ Assets/Scripts/Screen/Screen.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+// A screen is a single window that contains the Canvas for drawing to the screen as well as any images and buttons etc that are displayed on that screen.
+public class Screen : MonoBehaviour
+{
+    //public GameObject[] objectsToAnimate;
+    //public GameObject[] buttons;
+    //public Vector3[] originalLocations;
+    //public Vector3[] newLocations;
+    //public bool onScreen; 
+
+    public Button firstActive;
+
+	// Use this for initialization
+	void Start ()
+    {
+        //ScreenManager.instance.eventHandler.firstSelectedGameObject = firstActive.gameObject;
+        //Button[] b = gameObject.GetComponentsInChildren<Button>();
+        //buttons = new GameObject[b.Length];
+        //for (int i = 0; i < b.Length; i++)
+        //{
+        //    buttons[i] = b[i].gameObject;
+
+        //}
+    }
+	
+    void OnEnable()
+    {
+        //ScreenManager.instance.eventHandler.firstSelectedGameObject = firstActive.gameObject;
+    }
+
+    void OnDisable()
+    {
+
+    }
+
+    public void SwitchScreens(GameObject to)
+    {
+        ScreenManager.instance.SwitchScreens(gameObject, to);
+    }
+}
---
+++ Assets/Scripts/Screen/ScreenManager.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ScreenManager : MonoBehaviour
+{
+    public static ScreenManager instance;
+    public EventSystem eventHandler;
+    public GameObject lastScreen;
+    public GameObject currentScreen;
+
+    //public float delay = 0.2f;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+	// Use this for initialization
+	void Start ()
+    {
+        instance = this;
+        eventHandler = FindObjectOfType<EventSystem>();
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+	
+	}
+
+    public void SwitchScreens(GameObject from, GameObject to)
+    {
+        to.SetActive(true);
+        currentScreen = to;
+        ScreenManager.instance.eventHandler.SetSelectedGameObject(to.GetComponent<Screen>().firstActive.gameObject);
+
+        lastScreen = from;
+        from.SetActive(false);
+    }
+
+    public void EnableObject(GameObject enable)
+    {
+        enable.SetActive(true);
+    }
+
+    public void DisableObject(GameObject disable)
+    {
+        disable.SetActive(true);
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        // Transition out here
+
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitApplication()
+    {
+        Application.Quit();
+    }
+}
