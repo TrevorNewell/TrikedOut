f72ff619f2c0aa66b8daca9d343a6e066be1e147
dgreyleman <dgreyleman@aol.com>
2016-11-11 19:26:52 -0700

    Control Manager
    Started working on Control Manager, which can save control schemes to
    files and define custom button mapping for each different player.

---
+++ Assets/Scripts/ControlManager.cs
@@ -0,0 +1,240 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using XboxCtrlrInput;
+using System.IO;
+
+public class ControlManager : MonoBehaviour
+{
+    public ControlManager instance;
+
+    private Dictionary<string, XboxAxis> xboxStringToAxis;
+    private Dictionary<string, XboxButton> xboxStringToButton;
+    private Dictionary<string, KeyCode> keyboardStringToKey;
+
+    private Dictionary<XboxAxis, string> xboxAxisToString;
+    private Dictionary<XboxButton, string> xboxButtonToString;
+    private Dictionary<KeyCode, string> keyboardKeyToString;
+
+    public Dictionary<string, InputValues> playerControls;
+    public Dictionary<string, string> files;
+
+    void Awake()
+    {
+        instance = this;
+
+        xboxStringToAxis = new Dictionary<string, XboxAxis>();
+        xboxStringToButton = new Dictionary<string, XboxButton>();
+        keyboardStringToKey = new Dictionary<string, KeyCode>();
+
+        xboxAxisToString = new Dictionary<XboxAxis, string>();
+        xboxButtonToString = new Dictionary<XboxButton, string>();
+        keyboardKeyToString = new Dictionary<KeyCode, string>();
+
+        SetDefinitions();
+
+        playerControls = new Dictionary<string, InputValues>();
+        files = new Dictionary<string, string>();
+
+        playerControls.Add("P1", new InputValues("P1"));
+        playerControls.Add("P2", new InputValues("P2"));
+        playerControls.Add("P3", new InputValues("P3"));
+        playerControls.Add("P4", new InputValues("P4"));
+
+        ReadInputOptions("P1");
+        ReadInputOptions("P2");
+        ReadInputOptions("P3");
+        ReadInputOptions("P4");
+    }
+
+    void WriteInputOptions(string prefix)
+    {
+        string file = playerControls[prefix].GetXboxValues(xboxAxisToString, xboxButtonToString);
+
+        StreamWriter sw = new StreamWriter(Application.dataPath + "/Controls/" + prefix + "controls.txt");
+        sw.Write(file.ToCharArray());
+        sw.Close();
+    }
+
+    void ReadInputOptions(string prefix)
+    {
+        StreamReader file = null;
+        playerControls[prefix].ClearDefinitions();
+
+        try
+        {
+            file = new StreamReader(Application.dataPath + "/Controls/" + prefix + "controls.txt");
+        }
+        catch(FileNotFoundException e)
+        {
+            playerControls[prefix].SetDefaultValues();
+            WriteInputOptions(prefix);
+            return;
+        }
+
+        int state = 0;
+        while (!file.EndOfStream)
+        {
+            string line = file.ReadLine();
+            if (line.Equals("[XboxAxis]") || line.Equals("[XboxButton]"))
+            {
+                state++;
+                continue;
+            }
+            string[] lines = line.Split("=".ToCharArray());
+            if (state == 1) playerControls[prefix].xboxAxisValues.Add(lines[0], xboxStringToAxis[lines[1]]);
+            else if (state == 2) playerControls[prefix].xboxButtonValues.Add(lines[0], xboxStringToButton[lines[1]]);
+        }
+
+        file.Close();
+    }
+    
+	void Start () {
+	
+	}
+	
+
+	void Update () {
+
+    }
+    void SetDefinitions()
+    {
+        //String to Input
+
+        xboxStringToAxis.Add("LeftTrigger", XboxAxis.LeftTrigger);
+        xboxStringToAxis.Add("RightTrigger", XboxAxis.RightTrigger);
+        xboxStringToAxis.Add("LeftStickX", XboxAxis.LeftStickX);
+        xboxStringToAxis.Add("LeftStickY", XboxAxis.LeftStickY);
+        xboxStringToAxis.Add("RightStickX", XboxAxis.RightStickX);
+        xboxStringToAxis.Add("RightStickY", XboxAxis.RightStickY);
+
+        xboxStringToButton.Add("A", XboxButton.A);
+        xboxStringToButton.Add("B", XboxButton.B);
+        xboxStringToButton.Add("X", XboxButton.X);
+        xboxStringToButton.Add("Y", XboxButton.Y);
+        xboxStringToButton.Add("RightBumper", XboxButton.RightBumper);
+        xboxStringToButton.Add("LeftBumper", XboxButton.LeftBumper);
+        xboxStringToButton.Add("DPadUp", XboxButton.DPadUp);
+        xboxStringToButton.Add("DPadDown", XboxButton.DPadDown);
+        xboxStringToButton.Add("DPadLeft", XboxButton.DPadLeft);
+        xboxStringToButton.Add("DPadRight", XboxButton.DPadRight);
+        xboxStringToButton.Add("Start", XboxButton.Start);
+        xboxStringToButton.Add("Back", XboxButton.Back);
+        xboxStringToButton.Add("LeftStick", XboxButton.LeftStick);
+        xboxStringToButton.Add("RightStick", XboxButton.RightStick);
+
+        keyboardStringToKey.Add("A", KeyCode.A);
+        keyboardStringToKey.Add("B", KeyCode.B);
+        keyboardStringToKey.Add("C", KeyCode.C);
+        keyboardStringToKey.Add("D", KeyCode.D);
+        keyboardStringToKey.Add("E", KeyCode.E);
+        keyboardStringToKey.Add("F", KeyCode.F);
+        keyboardStringToKey.Add("G", KeyCode.G);
+        keyboardStringToKey.Add("H", KeyCode.H);
+        keyboardStringToKey.Add("I", KeyCode.I);
+        keyboardStringToKey.Add("J", KeyCode.J);
+        keyboardStringToKey.Add("K", KeyCode.K);
+        keyboardStringToKey.Add("L", KeyCode.L);
+        keyboardStringToKey.Add("M", KeyCode.M);
+        keyboardStringToKey.Add("N", KeyCode.N);
+        keyboardStringToKey.Add("O", KeyCode.O);
+        keyboardStringToKey.Add("P", KeyCode.P);
+        keyboardStringToKey.Add("Q", KeyCode.Q);
+        keyboardStringToKey.Add("R", KeyCode.R);
+        keyboardStringToKey.Add("S", KeyCode.S);
+        keyboardStringToKey.Add("T", KeyCode.T);
+        keyboardStringToKey.Add("U", KeyCode.U);
+        keyboardStringToKey.Add("V", KeyCode.V);
+        keyboardStringToKey.Add("W", KeyCode.W);
+        keyboardStringToKey.Add("X", KeyCode.X);
+        keyboardStringToKey.Add("Y", KeyCode.Y);
+        keyboardStringToKey.Add("Z", KeyCode.Z);
+        keyboardStringToKey.Add("1", KeyCode.Alpha1);
+        keyboardStringToKey.Add("2", KeyCode.Alpha2);
+        keyboardStringToKey.Add("3", KeyCode.Alpha3);
+        keyboardStringToKey.Add("4", KeyCode.Alpha4);
+        keyboardStringToKey.Add("5", KeyCode.Alpha5);
+        keyboardStringToKey.Add("6", KeyCode.Alpha6);
+        keyboardStringToKey.Add("7", KeyCode.Alpha7);
+        keyboardStringToKey.Add("8", KeyCode.Alpha8);
+        keyboardStringToKey.Add("9", KeyCode.Alpha9);
+        keyboardStringToKey.Add("0", KeyCode.Alpha0);
+        keyboardStringToKey.Add("Tab", KeyCode.Tab);
+        keyboardStringToKey.Add("LeftShift", KeyCode.LeftShift);
+        keyboardStringToKey.Add("RightShift", KeyCode.RightShift);
+        keyboardStringToKey.Add("Space", KeyCode.Space);
+        keyboardStringToKey.Add("Return", KeyCode.Return);
+        keyboardStringToKey.Add("UpArrow", KeyCode.UpArrow);
+        keyboardStringToKey.Add("DownArrow", KeyCode.DownArrow);
+        keyboardStringToKey.Add("LeftArrow", KeyCode.LeftArrow);
+        keyboardStringToKey.Add("RightArrow", KeyCode.RightArrow);
+
+        //InputToString
+
+        xboxAxisToString.Add(XboxAxis.LeftTrigger, "LeftTrigger");
+        xboxAxisToString.Add(XboxAxis.RightTrigger, "RightTrigger");
+        xboxAxisToString.Add(XboxAxis.LeftStickX, "LeftStickX");
+        xboxAxisToString.Add(XboxAxis.LeftStickY, "LeftStickY");
+        xboxAxisToString.Add(XboxAxis.RightStickX, "RightStickX");
+        xboxAxisToString.Add(XboxAxis.RightStickY, "RightStickY");
+
+        xboxButtonToString.Add(XboxButton.A, "A");
+        xboxButtonToString.Add(XboxButton.B, "B");
+        xboxButtonToString.Add(XboxButton.X, "X");
+        xboxButtonToString.Add(XboxButton.Y, "Y");
+        xboxButtonToString.Add(XboxButton.RightBumper, "RightBumper");
+        xboxButtonToString.Add(XboxButton.LeftBumper, "LeftBumper");
+        xboxButtonToString.Add(XboxButton.DPadUp, "DPadUp");
+        xboxButtonToString.Add(XboxButton.DPadDown, "DPadDown");
+        xboxButtonToString.Add(XboxButton.DPadLeft, "DPadLeft");
+        xboxButtonToString.Add(XboxButton.DPadRight, "DPadRight");
+        xboxButtonToString.Add(XboxButton.Start, "Start");
+        xboxButtonToString.Add(XboxButton.Back, "Back");
+        xboxButtonToString.Add(XboxButton.LeftStick, "LeftStick");
+        xboxButtonToString.Add(XboxButton.RightStick, "RightStick");
+
+        keyboardKeyToString.Add(KeyCode.A, "A");
+        keyboardKeyToString.Add(KeyCode.B, "B");
+        keyboardKeyToString.Add(KeyCode.C, "C");
+        keyboardKeyToString.Add(KeyCode.D, "D");
+        keyboardKeyToString.Add(KeyCode.E, "E");
+        keyboardKeyToString.Add(KeyCode.F, "F");
+        keyboardKeyToString.Add(KeyCode.G, "G");
+        keyboardKeyToString.Add(KeyCode.H, "H");
+        keyboardKeyToString.Add(KeyCode.I, "I");
+        keyboardKeyToString.Add(KeyCode.J, "J");
+        keyboardKeyToString.Add(KeyCode.K, "K");
+        keyboardKeyToString.Add(KeyCode.L, "L");
+        keyboardKeyToString.Add(KeyCode.M, "M");
+        keyboardKeyToString.Add(KeyCode.N, "N");
+        keyboardKeyToString.Add(KeyCode.O, "O");
+        keyboardKeyToString.Add(KeyCode.P, "P");
+        keyboardKeyToString.Add(KeyCode.Q, "Q");
+        keyboardKeyToString.Add(KeyCode.R, "R");
+        keyboardKeyToString.Add(KeyCode.S, "S");
+        keyboardKeyToString.Add(KeyCode.T, "T");
+        keyboardKeyToString.Add(KeyCode.U, "U");
+        keyboardKeyToString.Add(KeyCode.V, "V");
+        keyboardKeyToString.Add(KeyCode.W, "W");
+        keyboardKeyToString.Add(KeyCode.X, "X");
+        keyboardKeyToString.Add(KeyCode.Y, "Y");
+        keyboardKeyToString.Add(KeyCode.Z, "Z");
+        keyboardKeyToString.Add(KeyCode.Alpha1, "1");
+        keyboardKeyToString.Add(KeyCode.Alpha2, "2");
+        keyboardKeyToString.Add(KeyCode.Alpha3, "3");
+        keyboardKeyToString.Add(KeyCode.Alpha4, "4");
+        keyboardKeyToString.Add(KeyCode.Alpha5, "5");
+        keyboardKeyToString.Add(KeyCode.Alpha6, "6");
+        keyboardKeyToString.Add(KeyCode.Alpha7, "7");
+        keyboardKeyToString.Add(KeyCode.Alpha8, "8");
+        keyboardKeyToString.Add(KeyCode.Alpha9, "9");
+        keyboardKeyToString.Add(KeyCode.Alpha0, "0");
+        keyboardKeyToString.Add(KeyCode.Tab, "Tab");
+        keyboardKeyToString.Add(KeyCode.LeftShift, "LeftShift");
+        keyboardKeyToString.Add(KeyCode.RightShift, "RightShift");
+        keyboardKeyToString.Add(KeyCode.Return, "Return");
+        keyboardKeyToString.Add(KeyCode.UpArrow, "UpArrow");
+        keyboardKeyToString.Add(KeyCode.DownArrow, "DownArrow");
+        keyboardKeyToString.Add(KeyCode.LeftArrow, "LeftArrow");
+        keyboardKeyToString.Add(KeyCode.RightArrow, "RightArrow");
+    }
+}
---
+++ Assets/Scripts/InputValues.cs
@@ -0,0 +1,110 @@
+﻿using XboxCtrlrInput;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class InputValues
+{
+    public string prefix;
+    public bool useController = true;
+    public XboxController playerController;
+    public Dictionary<string, XboxAxis> xboxAxisValues;
+    public Dictionary<string, XboxButton> xboxButtonValues;
+    public Dictionary<string, KeyCode> keyboardButtonValues;
+
+    public InputValues(string p)
+    {
+        prefix = p;
+
+        switch (prefix)
+        {
+            case "P1":
+                playerController = XboxController.First;
+                break;
+            case "P2":
+                playerController = XboxController.Second;
+                break;
+            case "P3":
+                playerController = XboxController.Third;
+                break;
+            case "P4":
+                playerController = XboxController.Fourth;
+                break;
+        }
+
+        xboxAxisValues = new Dictionary<string, XboxAxis>();
+        xboxButtonValues = new Dictionary<string, XboxButton>();
+        keyboardButtonValues = new Dictionary<string, KeyCode>();
+    }
+
+    public string GetXboxValues(Dictionary<XboxAxis, string> xbA2S, Dictionary<XboxButton, string> xbB2S)
+    {
+        string retVal = "[XboxAxis]" + System.Environment.NewLine;
+
+        retVal += "leftPedal=";
+        retVal += xbA2S[xboxAxisValues["leftPedal"]] + System.Environment.NewLine;
+        retVal += "rightPedal=";
+        retVal += xbA2S[xboxAxisValues["rightPedal"]] + System.Environment.NewLine;
+        retVal += "steerX=";
+        retVal += xbA2S[xboxAxisValues["steerX"]] + System.Environment.NewLine;
+        retVal += "steerY=";
+        retVal += xbA2S[xboxAxisValues["steerY"]] + System.Environment.NewLine;
+        retVal += "cameraX=";
+        retVal += xbA2S[xboxAxisValues["cameraX"]] + System.Environment.NewLine;
+        retVal += "cameraY=";
+        retVal += xbA2S[xboxAxisValues["cameraY"]] + System.Environment.NewLine;
+
+        retVal += "[XboxButton]" + System.Environment.NewLine;
+        retVal += "switchWeapon=";
+        retVal += xbB2S[xboxButtonValues["switchWeapon"]] + System.Environment.NewLine;
+        retVal += "activateWeapon=";
+        retVal += xbB2S[xboxButtonValues["activateWeapon"]] + System.Environment.NewLine;
+        retVal += "activatePickup=";
+        retVal += xbB2S[xboxButtonValues["activatePickup"]] + System.Environment.NewLine;
+        retVal += "pause=";
+        retVal += xbB2S[xboxButtonValues["pause"]] + System.Environment.NewLine;
+        retVal += "map=";
+        retVal += xbB2S[xboxButtonValues["map"]] + System.Environment.NewLine;
+
+        return retVal;
+    }
+
+    public void ClearDefinitions()
+    {
+        xboxAxisValues.Clear();
+        xboxButtonValues.Clear();
+        keyboardButtonValues.Clear();
+    }
+
+    public void SetDefaultValues()
+    {
+        xboxAxisValues.Add("leftPedal", XboxAxis.LeftTrigger);
+        xboxAxisValues.Add("rightPedal", XboxAxis.RightTrigger);
+        xboxAxisValues.Add("steerX", XboxAxis.LeftStickX);
+        xboxAxisValues.Add("steerY", XboxAxis.LeftStickY);
+        xboxAxisValues.Add("cameraX", XboxAxis.RightStickX);
+        xboxAxisValues.Add("cameraY", XboxAxis.RightStickY);
+        //Drifting is leftPedal || rightPedal > 1 sec
+        //Breaking is leftPedal && rightPedal > 1 sec
+
+        xboxButtonValues.Add("switchWeapon", XboxButton.Y);
+        xboxButtonValues.Add("activateWeapon", XboxButton.A);
+        xboxButtonValues.Add("activatePickup", XboxButton.X);
+        xboxButtonValues.Add("pause", XboxButton.Start);
+        xboxButtonValues.Add("map", XboxButton.RightBumper);
+
+        //KEYBOARD
+        keyboardButtonValues.Add("leftPedalK", KeyCode.A);
+        keyboardButtonValues.Add("rightPedalK", KeyCode.D);
+        //Breaking is leftPedal && rightPedal
+        keyboardButtonValues.Add("switchWeaponK", KeyCode.UpArrow);
+        keyboardButtonValues.Add("activateWeaponK", KeyCode.W);
+        keyboardButtonValues.Add("activatePickupK", KeyCode.S);
+        //Drifting is leftPedal || rightPedal
+        keyboardButtonValues.Add("pauseK", KeyCode.Escape);
+        keyboardButtonValues.Add("mapK", KeyCode.M);
+        keyboardButtonValues.Add("steerXK", KeyCode.LeftArrow);
+        keyboardButtonValues.Add("steerYK", KeyCode.RightArrow);
+        keyboardButtonValues.Add("cameraXK", KeyCode.Tab);
+        keyboardButtonValues.Add("cameraYK", KeyCode.LeftShift);
+    }
+}
