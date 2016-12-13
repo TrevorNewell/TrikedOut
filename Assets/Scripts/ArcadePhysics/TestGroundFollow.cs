37598a301182b259276c918622964a7f3f5ad777
TrevorNewell <trevor.newell@utah.edu>
2016-12-12 19:20:42 -0700

    Started working on an alternative to the current movement...
    ... and it's promising!! Already I've obtained much greater stability
    overall.  The only downside is we'll have to simulate gravity if and
    when it comes time, but for our current uses it's :+1:

---
+++ Assets/Scripts/ArcadePhysics/TestGroundFollow.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestGroundFollow : MonoBehaviour
+{
+    public float turnSpeed;
+    public float speed;
+
+    public CharacterController controller;
+    public float curDir = 0f; // compass indicating direction
+    public float vertSpeed = 0f; // vertical speed (see note)
+    public Vector3 curNormal = Vector3.up; // smoothed terrain normal
+
+    void Start()
+    {
+        controller = gameObject.GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        float turn = Input.GetAxis("Horizontal") * turnSpeed * 100 * Time.deltaTime;
+        curDir = (curDir + turn) % 360; // rotate angle modulo 360 according to input
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, -curNormal, out hit))
+        {
+            curNormal = Vector3.Lerp(curNormal, hit.normal, 4 * Time.deltaTime);
+            Quaternion grndTilt = Quaternion.FromToRotation(Vector3.up, curNormal);
+            transform.rotation = grndTilt * Quaternion.Euler(0, curDir, 0);
+            transform.position = new Vector3(transform.position.x, transform.position.y - hit.distance, transform.position.z); // This line forces the object to follow the terrain regardless of how fast the height is changing.  In the future we can put this in an if that lets us manually make the player jump (as is the case for when we are ulted, or if we want to send them on a jump.  If this is commented gravity is simulated but not too well.
+        }
+        Vector3 movDir;
+        movDir = transform.forward * Input.GetAxis("Vertical") * speed;
+        // moves the character in horizontal direction (gravity changed!)
+        if (controller.isGrounded) vertSpeed = 0; // zero v speed when grounded
+        vertSpeed -= 9.8f * 3 * Time.deltaTime; // apply gravity
+        movDir.y = vertSpeed; // keep the current vert speed
+        controller.Move(movDir * Time.deltaTime);
+    }
+}
