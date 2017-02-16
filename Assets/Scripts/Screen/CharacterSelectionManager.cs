81b2c17eb1ceb1faa1a9aa6c04226341532624b4
TrevorNewell <trevor.newell@utah.edu>
2017-02-16 05:59:04 -0700

    Working on Character Selection Screen

---
+++ Assets/Scripts/Screen/CharacterSelectionManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class CharacterSelectionManager : MonoBehaviour {
+
+    public GameObject invisibleMiddleButton;
+
+    public GameObject[] subscreens;
+    public int currentIndex = 0;
+
+    private int maxIndex;
+
+    public int playerPicking = 1;
+
+	// Use this for initialization
+	void Start ()
+    {
+        maxIndex = subscreens.Length-1;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetButtonDown("P" + playerPicking + "_A"))
+        {
+            // SaveCharacterSelection(playerPicking, currentIndex);
+        }
+	}
+
+    public void TransitionLeft()
+    {
+        //FindObjectOfType<EventSystem>().SetSelectedGameObject(null);
+        //FindObjectOfType<EventSystem>().SetSelectedGameObject(invisibleMiddleButton);
+        Debug.Log("Left");
+    }
+
+    public void TransitionRight()
+    {
+        //FindObjectOfType<EventSystem>().SetSelectedGameObject(null);
+        //FindObjectOfType<EventSystem>().SetSelectedGameObject(invisibleMiddleButton);
+        Debug.Log("Right");
+    }
+
+
+}
---
+++ Assets/Scripts/Screen/Highlighted.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Highlighted : MonoBehaviour, ISelectHandler
+{
+    public bool left = false;
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        if (left)
+        {
+            //SendMessageUpwards("TransitionLeft");
+        }
+        else
+        {
+            //SendMessageUpwards("TransitionRight");
+        }
+    }
+
+}
