09b2d5f9d363bd6f473a5500235ad142c4277206
dgreyleman <dgreyleman@aol.com>
2017-02-18 18:15:50 -0700

    Bats and Animations
    Very sppooky.

---
+++ Assets/Scripts/Character/GetAnchors.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetAnchors : MonoBehaviour
+{
+    public GameObject dominantHand;
+    public float dominantHandOrientation = 1f;
+    public GameObject otherHand;
+    public GameObject leftFoot;
+    public GameObject rightFoot;
+}
